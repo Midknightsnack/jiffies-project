2b893fcb2e7a645ada732b040df06b1c9051a17a
Midknightsnack <42824199+Midknightsnack@users.noreply.github.com>
2021-02-23 22:22:46 -0800

    Add files via upload

--- jiffies/jiffies.c
+++ jiffies/jiffies.c
@@ -1,1 +1,59 @@
+// creating proc called jiffies
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#include <linux/jiffies.h>
 
+#define BUFFER_SIZE 128
+#define PROC_NAME "jiffies"
+
+//function protoypes
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+    .owner = THIS_MODULE,
+    .read = proc_read,
+};
+
+//loading module
+int proc_init(void){
+    // creates the /proc/jiffies entry
+    proc_create(PROC_NAME, 0666, NULL, &proc_ops);
+    printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+	return 0;
+}
+
+//removing module
+void proc_exit(void){
+    // removes the /proc/jiffies entry
+    remove_proc_entry(PROC_NAME, NULL);
+    printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+///proc/jiffies is read
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+        int rv = 0;
+        char buffer[BUFFER_SIZE];
+        static int completed = 0;
+
+        if (completed) {
+            completed = 0;
+            return 0;
+        }
+
+        completed = 1;
+        rv = sprintf(buffer, "The current value of jiffies is %lu\n", jiffies);
+        copy_to_user(usr_buf, buffer, rv);
+        return rv;
+}
+
+
+module_init( proc_init );
+module_exit( proc_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Jiffies Module");
+MODULE_AUTHOR("Bradley Diep");
